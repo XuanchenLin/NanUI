db56d48216d3aacec445a14ae58836e9b941ecbb
林选臣 <xuanchenlin@msn.com>
2016-06-08 17:02:47 +0800

    修复了一些问题

--- NanUI.Demo.Welcome/Program.cs
+++ NanUI.Demo.Welcome/Program.cs
@@ -21,16 +21,7 @@ namespace NanUI.Demo.Welcome
 
 			HtmlUILauncher.EnableFlashSupport = true;
 
-			if (HtmlUILauncher.InitializeChromium(args =>
-			{
-				args.Settings.LogSeverity = Chromium.CfxLogSeverity.Default;
-				//这里可以对CEF进行一些设定
-
-			}, args =>
-			{
-				Console.WriteLine(args.CommandLine);
-				//输出命令行开关，看看启用或是禁用了哪些功能
-			}))
+			if (HtmlUILauncher.InitializeChromium())
 			{
 				//初始化成功，加载程序集内嵌的资源到运行时中
 				HtmlUILauncher.RegisterEmbeddedScheme(System.Reflection.Assembly.GetExecutingAssembly());
--- NetDimension.NanUI/HtmlUILauncher.cs
+++ NetDimension.NanUI/HtmlUILauncher.cs
@@ -163,6 +163,12 @@ namespace NetDimension.NanUI
 				["snapshot_blob.bin"] = System.IO.File.Exists(System.IO.Path.Combine(FrameworkDir, "snapshot_blob.bin"))
 			};
 
+			if (EnableFlashSupport)
+			{
+				environmentDetectResults["manifest.json"] = System.IO.File.Exists(System.IO.Path.Combine(FrameworkDir, "PepperFlash\\manifest.json"));
+				environmentDetectResults["pepflashplayer.dll"] = System.IO.File.Exists(System.IO.Path.Combine(FrameworkDir, "PepperFlash\\pepflashplayer.dll"));
+			}
+
 			return environmentDetectResults.Count(p => p.Value == true) == environmentDetectResults.Count;
 
 		}
@@ -206,7 +212,6 @@ namespace NetDimension.NanUI
 				var cachePath = System.IO.Path.Combine(RuntimeDir, Application.ProductName, "Cache");
 				if (!System.IO.Directory.Exists(cachePath))
 					System.IO.Directory.CreateDirectory(cachePath);
-
 
 				args.Settings.LocalesDirPath = LocalesDir; 
 				args.Settings.ResourcesDirPath = ResourcesDir;
