d60c27eb6cede60743c50680888d04e30fd49892
林选臣 <xuanchenlin@msn.com>
2016-06-12 23:52:41 +0800

    添加了新的demo程序，Markdown编辑器

---
+++ NanUI.Demo.MarkdownDotNet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace NanUI.Demo.MarkdownDotNet
+{
+	using NetDimension.NanUI;
+
+	static class Program
+	{
+		/// <summary>
+		/// 应用程序的主入口点。
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			if (HtmlUILauncher.InitializeChromium())
+			{
+				HtmlUILauncher.RegisterEmbeddedScheme(System.Reflection.Assembly.GetExecutingAssembly(),"res");
+				Application.Run(new frmMain());
+			}
+		}
+	}
+}
