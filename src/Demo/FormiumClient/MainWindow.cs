7281bef15a9fd798d8ee7b8dc465ddbaf63f3c36
林选臣 <xuanchenlin@msn.com>
2022-01-16 00:33:20 +0800

    👾 修复了一个Bug：当路径中有emoji表情的时候报找不到CEF的问题。

--- src/Demo/FormiumClient/MainWindow.cs
+++ src/Demo/FormiumClient/MainWindow.cs
@@ -1,12 +1,19 @@
 using NetDimension.NanUI;
+using NetDimension.NanUI.Browser;
+using NetDimension.NanUI.Browser.ResourceHandler;
 using NetDimension.NanUI.HostWindow;
 using NetDimension.NanUI.JavaScript;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Xilium.CefGlue;
 
 namespace FormiumClient;
 
@@ -123,6 +130,27 @@ class MainWindow : Formium
         // Register event handler when keyboard is pressed
         // 注册按键事件
         KeyEvent += MainWindow_KeyEvent;
+
+        ShowDevTools();
+
+        
+    }
+
+    protected override void OnGetResourceRequestHandler(GetResourceRequestHandlerEventArgs e)
+    {
+        //var uri = new Uri(e.Request.Url);
+        //if(uri.LocalPath == "/api/test") // <--- 设置拦截器
+        //{
+        //    e.DisableDefaultHandling = true;
+
+        //    e.Handler = new MyRequestHandler(); ;
+
+        //    return;
+        //}
+
+        e.Handler = new MyRequestHandler();
+
+        base.OnGetResourceRequestHandler(e);
     }
 
     #region Events
@@ -502,4 +530,106 @@ return {
     #region JavaScript Window Binding
 
     #endregion
+}
+
+class MyCookieFilter : CefCookieAccessFilter
+{
+    protected override bool CanSaveCookie(CefBrowser browser, CefFrame frame, CefRequest request, CefResponse response, CefCookie cookie)
+    {
+        return true;
+    }
+
+    protected override bool CanSendCookie(CefBrowser browser, CefFrame frame, CefRequest request, CefCookie cookie)
+    {
+        return true;
+    }
+
+}
+
+class MyRequestHandler : CefResourceRequestHandler
+{
+    protected override CefCookieAccessFilter GetCookieAccessFilter(CefBrowser browser, CefFrame frame, CefRequest request)
+    {
+        return new MyCookieFilter();
+    }
+        //protected override CefResourceHandler GetResourceHandler(CefBrowser browser, CefFrame frame, CefRequest request)
+    //{
+    //    var handler = new MyResourceHandler(); // <------------ 拦截请求，用自定义的资源控制器替换。
+
+    //    return handler; 
+
+
+    //    //return base.GetResourceHandler(browser, frame, request);
+    //}
+
+    protected override CefResponseFilter GetResourceResponseFilter(CefBrowser browser, CefFrame frame, CefRequest request, CefResponse response)
+    {
+        return new MyFilter();
+    }
+
+}
+class MyFilter : CefResponseFilter
+{
+    MemoryStream memoryStream;
+    protected override CefResponseFilterStatus Filter(UnmanagedMemoryStream dataIn, long dataInSize, out long dataInRead, UnmanagedMemoryStream dataOut, long dataOutSize, out long dataOutWritten)
+    {
+        if (dataIn == null)
+        {
+            dataInRead = 0;
+            dataOutWritten = 0;
+
+            return CefResponseFilterStatus.Done;
+        }
+
+        dataInRead = dataIn.Length;
+        dataOutWritten = Math.Min(dataInRead, dataOutSize);
+
+        dataIn.CopyTo(dataOut);
+
+
+
+        dataIn.Position = 0;
+        dataIn.CopyTo(memoryStream);
+
+        //var buff = memoryStream.ToArray();
+
+        ////var str = Encoding.UTF8.GetString(buff);
+
+        //Debug.WriteLine(BitConverter.ToString(buff));
+
+        return CefResponseFilterStatus.Done;
+    }
+
+    protected override bool InitFilter()
+    {
+        memoryStream = new MemoryStream();
+        return true;
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            memoryStream?.Dispose();
+            memoryStream = null;
+        }
+        base.Dispose(disposing);
+
+
+    }
+}
+
+class MyResourceHandler : ResourceHandlerBase
+{
+    protected override ResourceResponse GetResourceResponse(ResourceRequest request)
+    {
+        var response = new ResourceResponse(request);
+
+        response.Content(JsonConvert.SerializeObject(new {
+            Text = "Hello",
+            Parameters = "World"
+        }, new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() }), CefRuntime.GetMimeType("json"));
+
+        return response; // <---- 返回自定义资源
+    }
 }
--- src/NetDimension.NanUI/third_party_libraries/CefGlue-90.6.7_g19ba721_chromium_90.0.4430.212/Platform/Windows/NativeMethods.cs
+++ src/NetDimension.NanUI/third_party_libraries/CefGlue-90.6.7_g19ba721_chromium_90.0.4430.212/Platform/Windows/NativeMethods.cs
@@ -10,6 +10,6 @@ internal static partial class NativeMethods
     [DllImport("kernel32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
     public static extern IntPtr GetModuleHandle(string lpModuleName);
 
-    [DllImport("kernel32.dll", SetLastError = true)]
+    [DllImport("kernel32.dll", SetLastError = true, CharSet = System.Runtime.InteropServices.CharSet.Auto)]
     public static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hReservedNull, LoadLibraryFlags dwFlags);
 }
