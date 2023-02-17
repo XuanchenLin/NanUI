ccea8cf9a9fb6e3b4c6593072f2b132accddcf21
林选臣 <xuanchenlin@msn.com>
2023-02-17 23:20:58 +0800

    🎉 更新了前端项目的各种库版本

--- src/Demo/FormiumClientFrontends/startup-ui/src/App.js
+++ src/Demo/FormiumClientFrontends/startup-ui/src/App.js
@@ -1,36 +1,50 @@
-import './App.scss';
+import "./App.scss";
 import sound from "./assets/start.wav";
-import { useRef, useEffect, } from "react";
+import { useRef, useEffect } from "react";
 import { ReactComponent as StartIcon } from "./assets/start.svg";
 
 function App() {
+  const culture = window?.Formium?.culture.name || "en-US";
 
   const audioPlayer = useRef(null);
   useEffect(() => {
-
     const handle = setTimeout(() => {
       audioPlayer.current.play();
     }, 1100);
     return () => {
       clearTimeout(handle);
-    }
+    };
   }, []);
 
-  useEffect(() => {
+  let messages = {
+    close: "Close",
+    slogan: "Build .NET WinForm Apps with HTML/CSS/JS",
+    launch: "Launch Demo",
+  };
 
+  if (culture === "zh-CN") {
+    messages = {
+      close: "关闭窗口",
+      slogan: "用 HTML/CSS/JS 来创建 .NET 窗体应用",
+      launch: "开始体验",
+    };
+  }
+  useEffect(() => {
     const handle = setTimeout(() => {
       const rootEl = document.querySelector("html");
       rootEl.classList.add("animation-complete");
     }, 6000);
     return () => {
       clearTimeout(handle);
-    }
+    };
   }, []);
 
   return (
     <div className="App">
-      <div className="close-btn" title="Close" formium-command="close">&#x2715;</div>
-      <audio src={sound} ref={audioPlayer} style={{ display: 'none' }} />
+      <div className="close-btn" title={messages.close} formium-command="close">
+        &#x2715;
+      </div>
+      <audio src={sound} ref={audioPlayer} style={{ display: "none" }} />
       <div className="animate-squre one"></div>
       <div className="animate-squre two"></div>
       <div className="animate-squre three"></div>
@@ -46,21 +60,19 @@ function App() {
           <div className="nanui-brand">
             Nan<span>UI</span>
           </div>
-
         </div>
-        <div className="slogan">
-          A .NET UI Component for WinForms
-        </div>
-        <div className="buttons" title="Launch Demo">
-          <div className="start-btn" onClick={() => {
-            const { Formium } = window;
-            Formium?.external?.launcher?.launchDemo();
-          }}>
+        <div className="slogan">{messages.slogan}</div>
+        <div className="buttons" title={messages.launch}>
+          <div
+            className="start-btn"
+            onClick={() => {
+              const { Formium } = window;
+              Formium?.external?.launcher?.launchDemo();
+            }}
+          >
             <StartIcon />
           </div>
           <div className="btn-halo"></div>
-
-
         </div>
       </div>
     </div>
